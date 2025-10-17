86609e3ac083e4bd0bba7aa6e84f4095e866e41c
tgszy <sunjinyuanqwe@gmail.com>
2025-10-18 00:35:53 +0800

    Update sys.py

--- src/sys.py
+++ src/sys.py
@@ -1,1 +1,11 @@
 
+from telegram.ext import CommandHandler
+
+def reboot(update, context):
+    update.message.reply_text("🦾 系统重启指令已发送")
+
+def update(update, context):
+    update.message.reply_text("⬆️ 正在拉取最新镜像并重启")
+
+reboot_handler = CommandHandler("reboot", reboot)
+update_handler = CommandHandler("update", update)
